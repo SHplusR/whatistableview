af0d46478df9df7b1bb140765f101e829c83f4b9
HYORISIM <devyousim@gmail.com>
2022-09-20 19:12:01 +0900

    first commit

---
+++ whatistableview/CustomTableVIewCellTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTableVIewCellTableViewCell.swift
+//  whatistableview
+//
+//  Created by HYORI SIM on 2022/09/20.
+//
+
+import UIKit
+
+class CustomTableVIewCellTableViewCell: UITableViewCell {
+
+    @IBAction func checkbox(_ sender: UIButton) {
+    }
+    
+}
